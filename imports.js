b7429a9aa55b4083f645eaf5608ebb59b08f916c
naveensaigit <naveensaisreenivas@gmail.com>
2022-06-23 17:50:04 +0530

    Code for parsing imports

---
+++ imports.js
@@ -0,0 +1,82 @@
+import 
+defaultExport from 'module';
+
+import {named1, alias2 as named2} from 'module';
+
+import * as 
+namespaceExport from
+ 'module';
+
+import 
+* 
+as 
+namespaceExport,
+ {named1, 
+    alias2 
+    as 
+named2} 
+from 'module';
+
+import          {named1
+                , alias2
+ as named2}
+ , 
+    * as 
+    namespaceExport     from        'module';
+
+import      
+defaultExport      
+, * as 
+namespaceExport 
+from 'module';
+
+import defaultExport,     {named1, 
+alias2 as named2} 
+from 
+'module';
+
+import * as namespaceExport,      defaultExport
+     from      'module';
+
+import 
+{named1, alias2 as named2},
+
+defaultExport from 
+'module';
+
+import    
+defaultExport            ,
+
+*
+as
+namespaceExport, {named1   
+, alias2 as named2, 
+...} from 'module';
+
+import
+
+defaultExport
+, {named1, alias2 as named2, ...}, * as namespaceExport from 'module';
+
+import 
+* as namespaceExport       , defaultExport      ,
+{named1, 
+alias2 as 
+named2} from 'module';
+
+import * as namespaceExport,
+{named1, alias2 as named2},
+defaultExport from 'module';
+
+import {named1, alias2 as named2}
+
+
+,defaultExport
+, * as namespaceExport from 'module';
+
+import {named1, alias2 as named2}, * as namespaceExport
+,
+
+ defaultExport
+from 
+'module';
---
+++ parseImports.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+let filePath = "";
+
+process.argv.forEach(function (val, index, _) {
+  if (index === 2) filePath = val;
+});
+
+function removeComments(data) {
+  let regex = /\/\/.*/g;
+  data = data.replace(regex, "");
+  regex = /\/\*(\s|.|\r\n)*?\*\//gm;
+  data = data.replace(regex, "");
+
+  return data;
+}
+
+function removeFileImports(text) {
+  let reg = /import((\r\n|\s)*?)?('|")(.*)('|");/gm;
+  return text.replace(reg, "");
+}
+
+let imports = [];
+
+function getImports(text) {
+  text = removeComments(text);
+  text = removeFileImports(text);
+
+  let reg = new RegExp("import((.|\r\n|\\s)*?)?from((.|\r\n|\\s)*?)?;", "gm");
+  do {
+    match = reg.exec(text);
+    if (match) {
+      let module = match[3].match(/('|")(.*)('|")/gm)[0];
+      let stmt = match[0].trim().replaceAll("\n", "");
+
+      let namedExps = new RegExp("{((.|\r\n|\\s)*?)}", "gm");
+      namedExps = namedExps.exec(stmt);
+      if (namedExps) {
+        namedExps = namedExps[1].split(",");
+        namedExps = namedExps.map(e => {
+          e = e.replaceAll("\n", "").split(" ");
+          let alias = '', namedExp = '';
+          for (let i of e) {
+            if (i != '' && i != 'as') {
+              if (alias == '')
+                alias = i;
+              else
+                namedExp = i;
+            }
+          }
+          if (namedExp == '')
+            return { namedExp: alias };
+          return { alias, namedExp };
+        });
+      }
+
+      let defaultExp = new RegExp("(import|,)((\r\n|\\s)*?)?(\\w*)((\r\n|\\s)*?)?(,|from)", "gm");
+      defaultExp = defaultExp.exec(stmt);
+      if (defaultExp)
+        defaultExp = defaultExp[4].trim().replaceAll("\n", "");
+
+      let namespaceExp = new RegExp("\\*((.|\r\n|\\s)*?)?as((.|\r\n|\\s)*?)?((.|\r\n|\\s)*?)(,|from)", "gm");
+      namespaceExp = namespaceExp.exec(stmt);
+      if (namespaceExp)
+        namespaceExp = namespaceExp[5].trim().replaceAll("\n", "");
+
+      imports.push({
+        import: stmt,
+        defaultExp,
+        namedExps: JSON.stringify(namedExps),
+        namespaceExp,
+        module
+      });
+    }
+  } while (match);
+}
+
+fs.readFile(filePath, (err, e) => {
+  if (err) throw err;
+
+  let data = e.toString();
+  getImports(data);
+  console.log(imports);
+});
