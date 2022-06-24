3e007a0289cb0792c7f2e2eb417aca993d1edec6
naveensaigit <naveensaisreenivas@gmail.com>
2022-06-24 21:19:36 +0530

    Organized code for parseImports

--- parseImports.js
+++ parseImports.js
@@ -1,83 +1,124 @@
-const fs = require("fs");
-let filePath = "";
+// Parsing import statements in a file
 
-process.argv.forEach(function (val, index, _) {
-  if (index === 2) filePath = val;
-});
-
-function removeComments(data) {
+// Remove comments present in file
+export function removeComments(data) {
+  // Regex to find single line comments - Starts with //
   let regex = /\/\/.*/g;
+  // Replace these comments with empty string
   data = data.replace(regex, "");
+
+  // Regex to find multi-line comments
+  // Starts with /* and ends with */
   regex = /\/\*(\s|.|\r\n)*?\*\//gm;
+  // Replace these comments with empty string
   data = data.replace(regex, "");
 
   return data;
 }
 
-function removeFileImports(text) {
-  let reg = /import((\r\n|\s)*?)?('|")(.*)('|");/gm;
+// Remove single file imports and existing dynamic imports
+export function removeImports(text) {
+  // Remove imports of the type -
+  // 1. File import - import "filename";
+  // 2. Dynamic import - import("filename")
+  let reg = /import((\r\n|\s|\()*?)?('|")(.*)('|")(\))*;/gm;
+  // Replace these imports with empty string
   return text.replace(reg, "");
 }
 
-let imports = [];
+// Get named exports section from an import statement
+export function getNamedExps(stmt) {
+  // Named exports section is enclosed within { ... }
+  let namedExps = new RegExp("{((.|\r\n|\\s)*?)}", "gm");
+  namedExps = namedExps.exec(stmt);
 
-function getImports(text) {
-  text = removeComments(text);
-  text = removeFileImports(text);
+  if (namedExps) {
+    // Taking the second element to get the second group from
+    // regex which contains the contents inside { ... }
 
-  let reg = new RegExp("import((.|\r\n|\\s)*?)?from((.|\r\n|\\s)*?)?;", "gm");
-  do {
-    match = reg.exec(text);
-    if (match) {
-      let module = match[3].match(/('|")(.*)('|")/gm)[0];
-      let stmt = match[0];
+    // Split string by , to get each export individually
+    namedExps = namedExps[1].split(",");
+    // Convert the string into an object
+    namedExps = namedExps.map(stringToNamedExps);
+  }
 
-      let namedExps = new RegExp("{((.|\r\n|\\s)*?)}", "gm");
-      namedExps = namedExps.exec(stmt);
-      if (namedExps) {
-        namedExps = namedExps[1].split(",");
-        namedExps = namedExps.map(e => {
-          e = e.replaceAll("\n", "").split(" ");
-          let alias = '', namedExp = '';
-          for (let i of e) {
-            if (i != '' && i != 'as') {
-              if (alias == '')
-                alias = i;
-              else
-                namedExp = i;
-            }
-          }
-          if (namedExp == '')
-            return { namedExp: alias };
-          return { alias, namedExp };
-        });
-      }
-
-      let defaultExp = new RegExp("(import|,)((\r\n|\\s)*?)?(\\w*)((\r\n|\\s)*?)?(,|from)", "gm");
-      defaultExp = defaultExp.exec(stmt);
-      if (defaultExp)
-        defaultExp = defaultExp[4].trim().replaceAll("\n", "");
-
-      let namespaceExp = new RegExp("\\*((.|\r\n|\\s)*?)?as((.|\r\n|\\s)*?)?((.|\r\n|\\s)*?)(,|from)", "gm");
-      namespaceExp = namespaceExp.exec(stmt);
-      if (namespaceExp)
-        namespaceExp = namespaceExp[5].trim().replaceAll("\n", "");
-
-      imports.push({
-        import: stmt,
-        defaultExp,
-        namedExps: JSON.stringify(namedExps),
-        namespaceExp,
-        module
-      });
-    }
-  } while (match);
+  return namedExps;
 }
 
-fs.readFile(filePath, (err, e) => {
-  if (err) throw err;
+// Get default export from an import statement
+export function getDefaultExp(stmt) {
+  // Default export is preceded by either "import" or ","
+  // and succeeded by either "," or "from"
+  let defaultExp = new RegExp("(import|,)((\r\n|\\s)*?)?(\\w*)((\r\n|\\s)*?)?(,|from)", "gm");
+  defaultExp = defaultExp.exec(stmt);
 
-  let data = e.toString();
-  getImports(data);
-  console.log(imports);
-});
+  if (defaultExp)
+    // Taking the fourth element to get the fourth group
+    // from regex which contains the default export
+
+    // Remove trailing whitespaces and remove newlines
+    defaultExp = defaultExp[4].trim().replaceAll("\n", "");
+
+  return defaultExp;
+}
+
+// Get namespace export from an import statement
+export function getNamespaceExp(stmt) {
+  // Namespace export contains "* as". It can
+  // be succeeded by either a "," or "from"
+  let namespaceExp = new RegExp("\\*((.|\r\n|\\s)*?)?as((.|\r\n|\\s)*?)?((.|\r\n|\\s)*?)(,|from)", "gm");
+  namespaceExp = namespaceExp.exec(stmt);
+
+  if (namespaceExp)
+    // Taking the fifth element to get the fifth group
+    // from regex which contains the namespace export
+
+    // Remove trailing whitespaces and remove newlines
+    namespaceExp = namespaceExp[5].trim().replaceAll("\n", "");
+
+  return namespaceExp;
+}
+
+// Convert import statement to an object
+export function importToObj(imp) {
+  let module = imp[3].match(/('|")(.*)('|")/gm)[0];
+  let stmt = imp[0];
+
+  let namedExps = getNamedExps(stmt);
+
+  let defaultExp = getDefaultExp(stmt);
+
+  let namespaceExp = getNamespaceExp(stmt);
+
+  return {
+    import: stmt,         // Import statement
+    defaultExp,           // Default export present in the import statement
+    namedExps: namedExps, // Named exports present in the import statement
+    namespaceExp,         // Namespace export present in the import statement
+    module                // Name of module from which import is happening
+  };
+}
+
+// Get an array of import objects for each import statement in the code
+export function getImports(code) {
+  let imports = [], match;
+  // Remove comments from the code
+  code = removeComments(code);
+  // Remove single file imports and existing dynamic imports
+  code = removeImports(code);
+
+  // Import statements are of the form "import <imports> from <moduleName>;"
+  let reg = new RegExp("import((.|\r\n|\\s)*?)?from((.|\r\n|\\s)*?)?;", "gm");
+
+  do {
+    // Find each import
+    match = reg.exec(code);
+    if (match) {
+      // Convert the import statement into an object
+      let importObj = importToObj(match);
+      imports.push(importObj);
+    }
+  } while (match);
+
+  return imports;
+}
