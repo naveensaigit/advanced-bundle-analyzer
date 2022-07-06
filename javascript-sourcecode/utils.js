406023e855b28a221bc52847763b0ffbb73562a9
naveensaigit <naveensaisreenivas@gmail.com>
2022-07-06 11:14:16 +0530

    Convert parseRepo to ts

--- javascript-sourcecode/utils.js
+++
@@ -1,9 +0,0 @@
-// Remove newlines, consecutive and trailing whitespaces
-export function preprocess(str) {
-  return str.trim().replace(/\s\s+/g, " ").toString().replaceAll("\n", "");
-}
-// Remove comments present in file
-export function removeComments(data) {
-  let regex = /\/\/.*|\/\*[^]*\*\//g;
-  return data.replace(regex, "");
-}
--- src/parseImports.ts
+++ src/parseImports.ts
@@ -1,27 +1,35 @@
 import fs from "fs";
 import { preprocess, removeComments } from "./utils.js";
+
+type lazyImp = { import: string; lazyImp: string; module: string };
+type getLazy = { newcode: string; lazyImps: lazyImp[] };
+type RegEx = RegExpExecArray | null;
+
 // Function to get lazy imports and then remove them from code
-function getLazyImports(code) {
+function getLazyImports(code: string): getLazy {
   // Lazy import is of the form -
   // keyword varName = lazyFunc(() => import("module"))
   // where keyword can be var, let, const or , (multiple declarations in same line)
   // varName is a valid JavaScript variable containing alphanumeric, $ and _ chars
-  let reg = new RegExp(
+
+  let reg: RegExp = new RegExp(
     // eslint-disable-next-line no-control-regex
     "(var|let|const|,)(\\s|\r\n)+(([a-zA-Z]|$|_)([a-zA-Z]|$|_|[0-9])*)(\\s|\r\n)*=\\s*(.|\r\n)*?\\s*import\\s*(.|\r\n)*?\\s*\\(((.|\r\n|\\s)*?)\\)",
     "gm"
   );
-  let lazyImps = [],
-    match,
-    newcode = code;
+
+  let lazyImps: lazyImp[] = [],
+    match: RegEx,
+    newcode: string = code;
+
   do {
     // Find each import
     match = reg.exec(code);
     if (match) {
       // Convert the import statement into an object
-      let importLine = match[0],
-        lazyImp = match[3],
-        module = match[9].slice(1, -1);
+      let importLine: string = match[0],
+        lazyImp: string = match[3],
+        module: string = match[9].slice(1, -1);
       // Remove this existing dynamic imports from the code
       newcode = newcode.replace(importLine, "");
       // Add the dynamic import
@@ -32,122 +40,170 @@ function getLazyImports(code) {
       });
     }
   } while (match);
+
   // Return the new code and list of lazy imports
   return { newcode, lazyImps };
 }
+
 // Remove single file imports
-function removeImports(text) {
+function removeImports(text: string): string {
   // Remove file import - import "filename";
-  let reg = /import((\r\n|\s)*?)?('|")(.*)('|");/gm;
+  let reg: RegExp = /import((\r\n|\s)*?)?('|")(.*)('|");/gm;
   // Replace these imports with empty string
   return text.replace(reg, "");
 }
+
+type stringToNamedImp = {
+  namedImp: string;
+  alias?: string;
+};
+
 // Convert "NamedImp as Alias" => {namedImp, alias}
-function stringToNamedImps(imp) {
+function stringToNamedImps(imp: string): stringToNamedImp {
   // Trim the string and replace multiple spaces with single space
-  let impArray = preprocess(imp).split(" ");
+  let impArray: string[] = preprocess(imp).split(" ");
   return impArray.length === 1
     ? { namedImp: impArray[0] }
     : { namedImp: impArray[0], alias: impArray[2] };
 }
+
 // Get named imports section from an import statement
-function getNamedImps(stmt) {
+function getNamedImps(stmt: string): stringToNamedImp[] | null {
   // Named imports section is enclosed within { ... }
   // eslint-disable-next-line no-control-regex
-  let namedImpsExp = new RegExp("{((.|\r\n|\\s)*?)}", "gm");
-  let namedImps = namedImpsExp.exec(stmt);
+  let namedImpsExp: RegExp = new RegExp("{((.|\r\n|\\s)*?)}", "gm");
+  let namedImps: RegEx = namedImpsExp.exec(stmt);
+
   if (namedImps) {
     // Taking the second element to get the second group from
     // regex which contains the contents inside { ... }
+
     // Split string by , to get each import individually
-    let namedImpsString = namedImps[1].split(",");
+    let namedImpsString: string[] = namedImps[1].split(",");
     // Convert the string into an object
-    let newNamedImps = namedImpsString.map(stringToNamedImps);
+    let newNamedImps: stringToNamedImp[] = namedImpsString.map(stringToNamedImps);
     return newNamedImps;
   }
+
   return null;
 }
+
 // Get default import from an import statement
-function getDefaultImp(stmt) {
+function getDefaultImp(stmt: string): string | null {
   // Default import is preceded by either "import" or ","
   // and succeeded by either "," or "from"
-  let defaultImpRegEx = new RegExp(
+  let defaultImpRegEx: RegExp = new RegExp(
     // eslint-disable-next-line no-control-regex
     "(import|,)((\r\n|\\s)*?)?(\\w*)((\r\n|\\s)*?)?(,|from)",
     "gm"
   );
-  let defaultImp = defaultImpRegEx.exec(stmt);
+
+  let defaultImp: RegEx = defaultImpRegEx.exec(stmt);
+
   if (defaultImp) {
     // Taking the fourth element to get the fourth group
     // from regex which contains the default import
+
     // Remove trailing whitespaces and newlines
-    let defaultImpStr = preprocess(defaultImp[4]);
+    let defaultImpStr: string = preprocess(defaultImp[4]);
     return defaultImpStr;
   }
+
   return null;
 }
+
 // Get namespace import from an import statement
-function getNamespaceImp(stmt) {
+function getNamespaceImp(stmt: string): String | null {
   // Namespace import contains "* as". It can
   // be succeeded by either a "," or "from"
-  let namespaceImpRegEx = new RegExp(
+  let namespaceImpRegEx: RegExp = new RegExp(
     // eslint-disable-next-line no-control-regex
     "\\*((.|\r\n|\\s)*?)?as((.|\r\n|\\s)*?)?((.|\r\n|\\s)*?)(,|from)",
     "gm"
   );
-  let namespaceImp = namespaceImpRegEx.exec(stmt);
+
+  let namespaceImp: RegEx = namespaceImpRegEx.exec(stmt);
+
   if (namespaceImp) {
     // Taking the fifth element to get the fifth group
     // from regex which contains the namespace import
+
     // Remove trailing whitespaces and newlines
-    let namespaceImpStr = preprocess(namespaceImp[5].toString());
+    let namespaceImpStr: String = preprocess(namespaceImp[5].toString());
     return namespaceImpStr;
   }
+
   return null;
 }
+
+type imports = {
+  import: string;
+  defaultImp: string | null;
+  namedImps: stringToNamedImp[] | null;
+  namespaceImp: String | null;
+  module: string;
+} | null;
+
 // Convert import statement to an object
-function importToObj(imp) {
+function importToObj(imp: RegExpExecArray): imports {
   if (!imp) return null;
+
   let module = imp[3].match(/('|")(.*)('|")/gm);
-  let moduleStr = module === null || module === void 0 ? void 0 : module[0];
+  let moduleStr: string | undefined = module?.[0];
+
   if (!moduleStr) return null;
   moduleStr = moduleStr.slice(1, -1);
-  let stmt = imp[0];
-  let namedImps = getNamedImps(stmt);
-  let defaultImp = getDefaultImp(stmt);
-  let namespaceImp = getNamespaceImp(stmt);
+
+  let stmt: string = imp[0];
+
+  let namedImps: stringToNamedImp[] | null = getNamedImps(stmt);
+
+  let defaultImp: string | null = getDefaultImp(stmt);
+
+  let namespaceImp: String | null = getNamespaceImp(stmt);
+
   return {
-    import: stmt,
-    defaultImp,
-    namedImps: namedImps,
-    namespaceImp,
-    module, // Name of module from which import is happening
+    import: stmt, // Import statement
+    defaultImp, // Default import present in the import statement
+    namedImps: namedImps, // Named imports present in the import statement
+    namespaceImp, // Namespace import present in the import statement
+    module: moduleStr, // Name of module from which import is happening
   };
 }
+
+type returnGetImports = {
+  imports: imports[];
+  lazyImps: lazyImp[];
+}
+
 // Get an array of import objects for each import statement in the code
-export function getImports(filePath) {
+export function getImports(filePath: string): returnGetImports {
   // Read the file contents
-  const oldcode = fs.readFileSync(filePath, "utf8");
+  const oldcode: string = fs.readFileSync(filePath, "utf8");
   // Get Lazy loaded imports and remove them from code
-  let { newcode: code, lazyImps } = getLazyImports(oldcode);
+  let { newcode: code, lazyImps }: getLazy = getLazyImports(oldcode);
   // Declaring variables to store ordinary imports and regex matches
-  let imports = [],
-    match;
+  let imports: imports[] = [],
+    match: RegEx;
+
   // Remove comments from the code
   code = removeComments(code);
   // Remove single file imports
   code = removeImports(code);
+
   // Import statements are of the form "import <imports> from <moduleName>;"
   // eslint-disable-next-line no-control-regex
-  let reg = new RegExp("import((.|\r\n|\\s)*?)?from((.|\r\n|\\s)*?)?;", "gm");
+  let reg: RegExp = new RegExp("import((.|\r\n|\\s)*?)?from((.|\r\n|\\s)*?)?;", "gm");
+
   do {
     // Find each import
     match = reg.exec(code);
     if (match) {
       // Convert the import statement into an object
-      let importObj = importToObj(match);
+      let importObj: imports = importToObj(match);
       imports.push(importObj);
     }
   } while (match);
+
   return { imports, lazyImps };
 }
