f2c6e6d60c3633171b66b1e062f89ca232514373
Ankit.Ghildiyal@123 <ce19b005@iittp.ac.in>
2022-08-01 21:06:58 +0530

    Formatting code

--- src/jsxFunctions.ts
+++ src/jsxFunctions.ts
@@ -2,103 +2,103 @@ import ts from '../node_modules/typescript/lib/typescript.js';
 import path from 'path';
 
 const startNewExecution = (fileName: string) => {
-	const program = ts.createProgram([fileName], {
-		target: ts.ScriptTarget.ES2015,
-		module: ts.ModuleKind.CommonJS,
-	});
-	const typeChecker = program.getTypeChecker();
+  const program = ts.createProgram([fileName], {
+    target: ts.ScriptTarget.ES2015,
+    module: ts.ModuleKind.CommonJS,
+  });
+  const typeChecker = program.getTypeChecker();
 
-	const files = program
-		.getSourceFiles()
-		.filter(
-			(file: { fileName: string | string[]; isDeclarationFile: any; }) =>
-				file.fileName.indexOf('node_modules') === -1 &&
-				!file.isDeclarationFile,
-		);
+  const files = program
+    .getSourceFiles()
+    .filter(
+      (file: { fileName: string | string[]; isDeclarationFile: any; }) =>
+        file.fileName.indexOf('node_modules') === -1 &&
+        !file.isDeclarationFile,
+    );
 
-	const [selectedFile] = files.filter(
-		(file: { fileName: string; }) => path.normalize(file.fileName) === fileName,
-	);
+  const [selectedFile] = files.filter(
+    (file: { fileName: string; }) => path.normalize(file.fileName) === fileName,
+  );
 
-	return {
-		typeChecker,
-		selectedFile,
-	};
+  return {
+    typeChecker,
+    selectedFile,
+  };
 }
 
 type FunctionNode = ts.FunctionDeclaration | ts.MethodDeclaration;
 
 const isFunctionNode = (node: FunctionNode): boolean => {
-	return ts.isFunctionDeclaration(node) || ts.isMethodDeclaration(node) || ts.isArrowFunction(node);
+  return ts.isFunctionDeclaration(node) || ts.isMethodDeclaration(node) || ts.isArrowFunction(node);
 }
 
 const getFunctionNodes = (node: ts.Node): FunctionNode[] => {
-	return node.getChildren().reduce(
-		(acc: any[], child: any) => {
-			if (isFunctionNode(child as FunctionNode)) {
-				acc.push(child as FunctionNode);
-			}
+  return node.getChildren().reduce(
+    (acc: any[], child: any) => {
+      if (isFunctionNode(child as FunctionNode)) {
+        acc.push(child as FunctionNode);
+      }
 
-			return [...acc, ...getFunctionNodes(child as FunctionNode)];
-		},
-		[] as FunctionNode[],
-	);
+      return [...acc, ...getFunctionNodes(child as FunctionNode)];
+    },
+    [] as FunctionNode[],
+  );
 }
 
 type jsxFunctions = {
-	[key: string]: boolean
+  [key: string]: boolean
 };
 
 const filterJsxFunctionsFromNode = (
-	node: FunctionNode,
-	typeChecker: ts.TypeChecker
+  node: FunctionNode,
+  typeChecker: ts.TypeChecker
 ): jsxFunctions => {
-	const typeOfFunction = typeChecker.getTypeAtLocation(node);
-	const parentSym = typeChecker.getSymbolAtLocation(node.parent.getChildAt(0));
-	const jsxFuncs: jsxFunctions = {}
+  const typeOfFunction = typeChecker.getTypeAtLocation(node);
+  const parentSym = typeChecker.getSymbolAtLocation(node.parent.getChildAt(0));
+  const jsxFuncs: jsxFunctions = {}
 
-	typeOfFunction.getCallSignatures().map((signature: { getReturnType: () => any; }) => {
-		const type = signature.getReturnType();
-		// @ts-ignore
-		const element = type.symbol?.escapedName, jsx = type.symbol?.parent?.escapedName;
-		const props = type.getProperties();
-		// @ts-ignore
-		const reactElement = props[0]?.parent?.escapedName;
-		if (element === "Element" && jsx === "JSX" && reactElement === "ReactElement") {
-			const name = (
-				parentSym?.getEscapedName()?.toString() ||
-				parentSym?.getName()?.toString() ||
-				// @ts-ignore
-				node.localSymbol?.escapedName ||
-				// @ts-ignore
-				node.symbol?.escapedName
-			);
-			if (name !== "__function")
-				jsxFuncs[name] = true;
-		}
-	});
+  typeOfFunction.getCallSignatures().map((signature: { getReturnType: () => any; }) => {
+    const type = signature.getReturnType();
+    // @ts-ignore
+    const element = type.symbol?.escapedName, jsx = type.symbol?.parent?.escapedName;
+    const props = type.getProperties();
+    // @ts-ignore
+    const reactElement = props[0]?.parent?.escapedName;
+    if (element === "Element" && jsx === "JSX" && reactElement === "ReactElement") {
+      const name = (
+        parentSym?.getEscapedName()?.toString() ||
+        parentSym?.getName()?.toString() ||
+        // @ts-ignore
+        node.localSymbol?.escapedName ||
+        // @ts-ignore
+        node.symbol?.escapedName
+      );
+      if (name !== "__function")
+        jsxFuncs[name] = true;
+    }
+  });
 
-	return jsxFuncs;
+  return jsxFuncs;
 }
 
 const filterJsxFunctionsFromNodes = (
-	nodes: FunctionNode[],
-	typeChecker: ts.TypeChecker,
+  nodes: FunctionNode[],
+  typeChecker: ts.TypeChecker,
 ): jsxFunctions => {
-	return nodes.reduce(
-		(acc, node) => {
-			return {
-				...acc,
-				...filterJsxFunctionsFromNode(node, typeChecker)
-			}
-		},
-		{},
-	);
+  return nodes.reduce(
+    (acc, node) => {
+      return {
+        ...acc,
+        ...filterJsxFunctionsFromNode(node, typeChecker)
+      }
+    },
+    {},
+  );
 }
 
 export const getJsxFunctions = (fileName: string): jsxFunctions => {
-	const { selectedFile, typeChecker } = startNewExecution(fileName);
-	const allFunctions = getFunctionNodes(selectedFile);
+  const { selectedFile, typeChecker } = startNewExecution(fileName);
+  const allFunctions = getFunctionNodes(selectedFile);
 
-	return filterJsxFunctionsFromNodes(allFunctions, typeChecker);
+  return filterJsxFunctionsFromNodes(allFunctions, typeChecker);
 }
